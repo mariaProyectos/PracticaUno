6f9489468dd326597d252a4406935688787cd828
Danvgon <danvgon@gmail.com>
2015-02-26 13:55:45 +0100

    Añadido constructor

--- PracticaUno/src/Controlador/claseUno.java
+++ PracticaUno/src/Controlador/claseUno.java
@@ -1,7 +1,15 @@
 package Controlador;
 
 public class claseUno {
-String cadenaUno;
-int posicionCadena;
-int numeroCadena;
+	String cadenaUno;
+	int posicionCadena;
+	int numeroCadena;
+
+	public claseUno(String cadenaUno, int posicionCadena, int numeroCadena) {
+		super();
+		this.cadenaUno = cadenaUno;
+		this.posicionCadena = posicionCadena;
+		this.numeroCadena = numeroCadena;
+	}
+
 }
